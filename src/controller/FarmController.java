ae7c47be9b9bf6df23fbf70cbf49ebd268f10745
Unknown <mohammad444t@gmail.com>
2018-12-19 16:57:39 +0330

    listenForCommand() Written, ExitRequest Added, UML Updated

--- src/controller/FarmController.java
+++ src/controller/FarmController.java
@@ -1,5 +1,6 @@
 package controller;
 import model.*;
+import model.request.*;
 import view.View;
 
 public class FarmController {
@@ -8,70 +9,116 @@ public class FarmController {
     private CommandAnalyzer commandAnalyzer = new CommandAnalyzer();
 
     public void listenForCommand() {
-        String command = view.getInput();
-    }
-
-    public void addAction() {
-
-    }
-
-    public void buyAction() {
-
-    }
-
-    public void cageAction() {
-
-    }
-
-    public void clearAction() {
-
-    }
-
-    public void goAction() {
-
-    }
-
-    public void loadCustomAction() {
-
-    }
-
-    public void loadGameAction() {
-
-    }
-
-    public void pickUpAction() {
-
-    }
-
-    public void plantAction() {
-
-    }
-
-    public void printAction() {
-
-    }
-
-    public void runAction() {
+        boolean requestsOnTheWay = true;
+        while (requestsOnTheWay) {
+            String command = view.getInput().trim();
+            Request request = commandAnalyzer.getRequest(command);
+            if (request instanceof AddRequest){
+                addAction((AddRequest) request);
+            }else if (request instanceof BadRequest){
+                badAction((BadRequest) request);
+            }else if (request instanceof BuyRequest){
+                buyAction((BuyRequest) request);
+            }else if (request instanceof CageRequest){
+                cageAction((CageRequest) request);
+            }else if (request instanceof ClearRequest){
+                clearAction((ClearRequest) request);
+            }else if (request instanceof GoRequest){
+                goAction((GoRequest) request);
+            }else if (request instanceof LoadCustomRequest){
+                loadCustomAction((LoadCustomRequest) request);
+            }else if (request instanceof LoadGameRequest){
+                loadGameAction((LoadGameRequest) request);
+            }else if (request instanceof PickUpRequest){
+                pickUpAction((PickUpRequest) request);
+            }else if (request instanceof PlantRequest){
+                plantAction((PlantRequest) request);
+            }else if (request instanceof PrintRequest){
+                printAction((PrintRequest) request);
+            }else if (request instanceof RunRequest){
+                runAction((RunRequest) request);
+            }else if (request instanceof SaveGameRequest){
+                saveGameAction((SaveGameRequest) request);
+            }else if (request instanceof StartRequest) {
+                startAction((StartRequest) request);
+            }else if (request instanceof TurnRequest){
+                turnAction((TurnRequest) request);
+            }else if (request instanceof UpgradeRequest){
+                upgradeAction((UpgradeRequest) request);
+            }else if (request instanceof WellRequest){
+                wellAction((WellRequest) request);
+            }else if (request instanceof ExitRequest){
+                requestsOnTheWay = false;
+            }
+        }
 
     }
 
-    public void saveGameAction() {
+    public void addAction(AddRequest request) {
 
     }
 
-    public void startAction() {
+    public void badAction(BadRequest request) {
 
     }
 
-    public void turnAction() {
+    public void buyAction(BuyRequest request) {
 
     }
 
-    public void upgradeAction() {
+    public void cageAction(CageRequest request) {
 
     }
 
-    public void wellAction() {
+    public void clearAction(ClearRequest request) {
+
+    }
+
+    public void goAction(GoRequest request) {
+
+    }
+
+    public void loadCustomAction(LoadCustomRequest request) {
+
+    }
+
+    public void loadGameAction(LoadGameRequest request) {
+
+    }
+
+    public void pickUpAction(PickUpRequest request) {
+
+    }
+
+    public void plantAction(PlantRequest  request) {
+
+    }
+
+    public void printAction(PrintRequest request) {
+
+    }
+
+    public void runAction(RunRequest request) {
+
+    }
+
+    public void saveGameAction(SaveGameRequest request) {
+
+    }
+
+    public void startAction(StartRequest request) {
+
+    }
+
+    public void turnAction(TurnRequest request) {
+
+    }
+
+    public void upgradeAction(UpgradeRequest request) {
+
+    }
+
+    public void wellAction(WellRequest request) {
 
     }
 }
