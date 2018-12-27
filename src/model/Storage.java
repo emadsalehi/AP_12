7ed99403deb9a885e8dda4d2c29e5e658948c4f7
Unknown <mohammad444t@gmail.com>
2018-12-27 12:28:13 +0330

    calculateUsedCapacity Added to Storage. Some Getters And Setters Added too.

--- src/model/Storage.java
+++ src/model/Storage.java
@@ -24,4 +24,45 @@ public class Storage {
     public void setProducts(ArrayList<Product> products) {
         this.products = products;
     }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getUpgradePrice() {
+        return upgradePrice;
+    }
+
+    public void setUpgradePrice(int upgradePrice) {
+        this.upgradePrice = upgradePrice;
+    }
+
+    public int calculateUsedCapacity(){
+        int usedCapacity = 0;
+        for(Product p : products){
+            if (p instanceof PrimitiveProduct){
+                usedCapacity += ((PrimitiveProduct) p).getPrimitiveProductType().getDepotSize();
+            }else if (p instanceof  SecondaryProduct){
+                usedCapacity += ((SecondaryProduct) p).getSecondaryProductType().getDepotSize();
+            }
+        }
+        for (Animal a : animals){
+            if (a instanceof WildAnimal){
+                usedCapacity += ((WildAnimal) a).getWildAnimalType().getDepotSize();
+            }
+        }
+        return usedCapacity;
+    }
 }
