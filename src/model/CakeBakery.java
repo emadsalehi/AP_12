68a378da8841d3678081318bc86b5f7bb1ff1195
Emad Salehi <emad.s1178@yahoo.com>
2018-12-27 19:29:50 +0330

    WorkShop Written.
    TODOs is not completed.

--- src/model/CakeBakery.java
+++ src/model/CakeBakery.java
@@ -1,6 +1,17 @@
 package model;
 
 public class CakeBakery extends WorkShop {
+
+    public CakeBakery(int maxTimeToFinish, int throwedProductX, int throwedProductY, int numberOfProcessedProduct) {
+        //todo replace variables with number for each workshop.
+        super(maxTimeToFinish, throwedProductX, throwedProductY, numberOfProcessedProduct);
+    }
+
+    @Override
+    public int getRepairCost() {
+        return 0;
+    }
+
     @Override
     public SecondaryProduct getProduct() {
         SecondaryProduct secondaryProduct = new SecondaryProduct(SecondaryProductType.CAKE);
--- src/model/EggPowderPlant.java
+++ src/model/EggPowderPlant.java
@@ -1,6 +1,16 @@
 package model;
 
 public class EggPowderPlant extends WorkShop {
+    public EggPowderPlant(int maxTimeToFinish, int throwedProductX, int throwedProductY, int numberOfProcessedProduct) {
+        //todo replace variables with number for each workshop.
+        super(maxTimeToFinish, throwedProductX, throwedProductY, numberOfProcessedProduct);
+    }
+
+    @Override
+    public int getRepairCost() {
+        return 0;
+    }
+
     @Override
     public SecondaryProduct getProduct() {
         SecondaryProduct secondaryProduct = new SecondaryProduct(SecondaryProductType.EGG_POWDER);
