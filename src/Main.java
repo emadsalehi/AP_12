3f37fcf1400589b87a4b1e83a9fc89b18e09515a
Emad Salehi <emad.s1178@yahoo.com>
2018-12-28 22:23:13 +0330

    Main class modified.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import controller.FarmController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello AP!");
+        new FarmController().listenForCommand();
     }
 }
