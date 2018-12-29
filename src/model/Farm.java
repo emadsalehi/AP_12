b484deab8515b287714ddd5336713478b4d38658
SinaBolouki <sina.bolouki@gmail.com>
2018-12-29 11:57:55 +0330

    updateCell Product making added

--- src/model/Farm.java
+++ src/model/Farm.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Farm {
+    private final int maxPlantLevel = 5;
     private Cell[][] cells = new Cell[30][30];
     private int money = 1000;
     private Storage storage;
@@ -13,8 +14,7 @@ public class Farm {
     private Truck truck;
     private Level level;
     private int time;
-    private final int maxPlantLevel = 5;
-  
+
     public int getTime() {
         return time;
     }
@@ -24,30 +24,48 @@ public class Farm {
     }
 
     public void updateCells() {
-       displacer();
-       for (int i = 0 ; i < 30 ; i++) {
-           for (int j = 0 ; j < 30 ; j++) {
-               Cell cell = cells[i][j];
-               checkWildAndFarmCollision(cell);
-               checkWildAndProductCollision(cell);
-               catProductCollision(cell);
-               if (cell.isHasPlant()) {
-                   for (Animal animal : cell.getAnimals()) {
-                       if (animal instanceof FarmAnimal) {
-                           if (((FarmAnimal) animal).isHungry()) {
-                               ((FarmAnimal) animal).setTimeTillHungry(((FarmAnimal) animal).getTimeTillHungry() + 1);
-                               cell.setPlantLevel(cell.getPlantLevel()-1);
-                           }
-                           if (cell.getPlantLevel()<1) {
-                               cell.setPlantLevel(0);
-                               cell.setHasPlant(false);
-                               break;
-                           }
-                       }
-                   }
-               }
-           }
-       }
+        displacer();
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                Cell cell = cells[i][j];
+                checkWildAndFarmCollision(cell);
+                checkWildAndProductCollision(cell);
+                catProductCollision(cell);
+                for (Animal animal : cell.getAnimals()) {
+                    if (animal instanceof FarmAnimal) {
+                        if (cell.isHasPlant()) {
+                            if (((FarmAnimal) animal).isHungry()) {
+                                ((FarmAnimal) animal).setTimeTillHungry(((FarmAnimal) animal).getTimeTillHungry() + 1);
+                                cell.setPlantLevel(cell.getPlantLevel() - 1);
+                            }
+                            if (cell.getPlantLevel() < 1) {
+                                cell.setPlantLevel(0);
+                                cell.setHasPlant(false);
+                                break;
+                            }
+                        }
+                        ((FarmAnimal) animal).nextTurn();
+                        if (((FarmAnimal) animal).isReadyToProduce()) {
+                            ((FarmAnimal) animal).setReadyToProduce(false);
+                            ArrayList<Product> products = cell.getProducts();
+                            if (((FarmAnimal) animal).getFarmAnimalType().equals(FarmAnimalType.CHICKEN)) {
+                                products.add(new PrimitiveProduct(PrimitiveProductType.EGG));
+                                cell.setProducts(products);
+                            }
+                            else if (((FarmAnimal) animal).getFarmAnimalType().equals(FarmAnimalType.COW)) {
+                                products.add(new PrimitiveProduct(PrimitiveProductType.MILK));
+                                cell.setProducts(products);
+                            }
+                            else {
+                                products.add(new PrimitiveProduct(PrimitiveProductType.WOOL));
+                                cell.setProducts(products);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
     }
 
     public Cell checkWildAndFarmCollision(Cell cell) {
@@ -55,14 +73,14 @@ public class Farm {
         ArrayList<Animal> cellAnimals = cell.getAnimals();
         ArrayList<Animal> farmAnimals = new ArrayList<Animal>();
         ArrayList<Animal> wildAnimals = new ArrayList<Animal>();
-        for (Animal animal : cellAnimals){
-            if(animal instanceof FarmAnimal){
+        for (Animal animal : cellAnimals) {
+            if (animal instanceof FarmAnimal) {
                 farmAnimals.add(animal);
-            }else {
+            } else {
                 wildAnimals.add(animal);
             }
         }
-        if (!farmAnimals.isEmpty() && !wildAnimals.isEmpty()){
+        if (!farmAnimals.isEmpty() && !wildAnimals.isEmpty()) {
             cellAnimals.clear();
             cell.setAnimals(cellAnimals);
         }
@@ -73,12 +91,12 @@ public class Farm {
         ArrayList<Animal> cellAnimals = cell.getAnimals();
         ArrayList<Product> cellProducts = cell.getProducts();
         ArrayList<Animal> wildAnimals = new ArrayList<Animal>();
-        for (Animal animal : cellAnimals){
-            if (animal instanceof WildAnimal){
+        for (Animal animal : cellAnimals) {
+            if (animal instanceof WildAnimal) {
                 wildAnimals.add(animal);
             }
         }
-        if (!wildAnimals.isEmpty()){
+        if (!wildAnimals.isEmpty()) {
             cellProducts.clear();
             cell.setProducts(cellProducts);
         }
@@ -86,8 +104,8 @@ public class Farm {
     }
 
     public void displacer() {
-        for (int i = 0 ; i < 30 ; i++)
-            for (int j = 0 ; j < 30 ; j++)
+        for (int i = 0; i < 30; i++)
+            for (int j = 0; j < 30; j++)
                 for (Animal animal : this.cells[i][j].getAnimals()) {
                     this.cells[animal.getX()][animal.getY()].getAnimals().add(animal);
                     this.cells[i][j].getAnimals().remove(animal);
@@ -105,8 +123,8 @@ public class Farm {
     }
 
     public void irrigate(int x, int y) {
-        for (int i = x - 1 ; i <= x + 1 ; i++)
-            for (int j = y - 1 ; j <= y + 1 ; j++)
+        for (int i = x - 1; i <= x + 1; i++)
+            for (int j = y - 1; j <= y + 1; j++)
                 if (i >= 0 && i < 30 && j >= 0 && j < 30) {
                     this.cells[i][j].setPlantLevel(maxPlantLevel);
                     this.cells[i][j].setHasPlant(true);
@@ -117,29 +135,29 @@ public class Farm {
         Cell cell = cells[x][y];
         if (cell.getProducts().size() != 0) {
             ArrayList<Product> cellProducts = cell.getProducts();
-            Product pickedUpProduct = cellProducts.get(cellProducts.size()-1);
+            Product pickedUpProduct = cellProducts.get(cellProducts.size() - 1);
             ArrayList<Product> storageProducts = storage.getProducts();
             storageProducts.add(pickedUpProduct);
             storage.setProducts(storageProducts);
-            cellProducts.remove(cellProducts.size()-1);
+            cellProducts.remove(cellProducts.size() - 1);
             cell.setProducts(cellProducts);
         }
         //todo If product exist store it in storage and remove it from cells.
     }
 
     public boolean levelPassedChecker() {
-        if ( money > level.getRequiredMoney()) {
-            HashMap <Animal, Integer> requiredAnimals = level.getRequiredAnimals();
-            HashMap <Product, Integer> requiredProduct = level.getRequiredProduct();
+        if (money > level.getRequiredMoney()) {
+            HashMap<Animal, Integer> requiredAnimals = level.getRequiredAnimals();
+            HashMap<Product, Integer> requiredProduct = level.getRequiredProduct();
 
-            ArrayList<Animal> storageAnimals= storage.getAnimals();
+            ArrayList<Animal> storageAnimals = storage.getAnimals();
             ArrayList<Product> storageProducts = storage.getProducts();
 
             for (HashMap.Entry<Animal, Integer> entry : requiredAnimals.entrySet()) {
                 Integer numberOfAnimals = 0;
                 for (Animal animal : storageAnimals) {
                     if (animal.equals(entry.getKey())) {
-                        numberOfAnimals ++;
+                        numberOfAnimals++;
                     }
                 }
                 if (numberOfAnimals < entry.getValue()) {
@@ -150,7 +168,7 @@ public class Farm {
                 Integer numberOfProducts = 0;
                 for (Product product : storageProducts) {
                     if (product.equals(entry.getKey())) {
-                        numberOfProducts ++;
+                        numberOfProducts++;
                     }
                 }
                 if (numberOfProducts < entry.getValue()) {
@@ -174,7 +192,7 @@ public class Farm {
         this.money = money;
     }
 
-    public void addAnimal(Animal animal){
+    public void addAnimal(Animal animal) {
         ArrayList<Animal> animals = cells[animal.getX()][animal.getY()].getAnimals();
         animals.add(animal);
         cells[animal.getX()][animal.getY()].setAnimals(animals);
