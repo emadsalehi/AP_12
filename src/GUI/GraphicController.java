3996ffe69a301ba24c6772adb07cd71f661638fb
Emad Salehi <emad.s1178@yahoo.com>
2019-01-24 16:50:19 +0330

    newGame and newMenu added to GraphicController

--- src/GUI/GraphicController.java
+++ src/GUI/GraphicController.java
@@ -1,9 +1,27 @@
 package GUI;
 
+import controller.FarmController;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class GraphicController extends Application {
+
+    private Group menu = new Group();
+    private Group game = new Group();
+    private Scene scene = new Scene(menu, 800, 600);
+    private final String pathToBackGroundImage = "D:\\University\\AP\\Project\\AP_12\\src\\GUI\\Textures\\back.png";
+    private ImageView backGround = new ImageView(new Image(new FileInputStream(pathToBackGroundImage)));
+    private FarmController farmController = new FarmController();
+
+    public GraphicController() throws FileNotFoundException {
+    }
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +29,20 @@ public class GraphicController extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        newGame();
+        scene.setRoot(game);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void newMenu() {
 
     }
+
+    public void newGame() {
+        this.game.getChildren().clear();
+        this.game.getChildren().add(backGround);
+    }
+
 }
